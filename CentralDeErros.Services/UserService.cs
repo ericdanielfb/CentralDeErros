2fc49198f9e8e90a29c58c73249df99903dec758
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2020-08-04 02:44:55 -0300

    Adiciona UserService

---
+++ CentralDeErros.Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using CentralDeErros.Core;
+using CentralDeErros.Model.Models;
+using CentralDeErros.Services.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralDeErros.Services
+{
+    public class UserService : ServiceBase<User>
+    {
+        public UserService(CentralDeErrosDbContext context) : base(context)
+        {
+
+        }
+
+
+    }
+}
