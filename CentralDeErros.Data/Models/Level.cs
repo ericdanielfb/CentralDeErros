78b314c15a098995814aa486a87f09f9d3758a26
Marcelo Martins <dw.cv.mj@gmail.com>
2020-08-06 16:53:55 -0300

    Changing the relationship between level and error

--- CentralDeErros.Data/Models/Level.cs
+++ CentralDeErros.Data/Models/Level.cs
@@ -7,12 +7,12 @@ namespace CentralDeErros.Model.Models
     {
         public Level()
         {
-            this.Errors = new HashSet<Error>();
+            this.Occurrences = new HashSet<Occurrence>();
         }
 
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public ICollection<Error> Errors { get; set; }
+        public ICollection<Occurrence> Occurrences { get; set; }
     }
 }
