1cded3eb8f15db1cc2498688b6e09c7cbd8b3a9a
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2020-08-07 12:29:16 -0300

    fix: UserDTO

--- CentralDeErros.Transport/UserDTO.cs
+++ CentralDeErros.Transport/UserDTO.cs
@@ -9,11 +9,9 @@ namespace CentralDeErros.Transport
     public class UserDTO
     {
 
-        public int Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
-        public int ProfileId { get; set; }
+
 
     }
 }
