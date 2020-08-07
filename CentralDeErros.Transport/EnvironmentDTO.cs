cb28657b920327fa1da4d6994e713a2441a09cb7
Marcelo Martins <dw.cv.mj@gmail.com>
2020-08-07 17:21:55 -0300

    Feat: Add EnviromentDTO

---
+++ CentralDeErros.Transport/EnvironmentDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CentralDeErros.Transport
+{
+    class EnvironmentDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Phase { get; set; }
+    }
+}
