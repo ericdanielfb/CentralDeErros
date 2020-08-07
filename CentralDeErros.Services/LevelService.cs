75d90fb815cc7341d8be38b005bf8e3de4447a25
Marcelo Martins <dw.cv.mj@gmail.com>
2020-08-07 16:41:38 -0300

    Feat: Add LevelService

---
+++ CentralDeErros.Services/LevelService.cs
@@ -0,0 +1,14 @@
+﻿using CentralDeErros.Core;
+using CentralDeErros.Model.Models;
+using CentralDeErros.Services.Base;
+
+namespace CentralDeErros.Services
+{
+    public class LevelService : ServiceBase<Level>
+    {
+        public LevelService(CentralDeErrosDbContext context) : base(context) 
+        {
+        }
+
+    }
+}
