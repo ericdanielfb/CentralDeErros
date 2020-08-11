cf6b46a8e93571a016a1a0f5b62dbcb694b63935
Marcelo Martins <dw.cv.mj@gmail.com>
2020-08-11 00:45:25 -0300

    Feat: JWT configuration

--- CentralDeErros.API/Configuration/IdentityConfig.cs
+++ CentralDeErros.API/Configuration/IdentityConfig.cs
@@ -1,13 +1,17 @@
 ﻿
+using CentralDeErros.API.Extensions;
 using CentralDeErros.Core;
 using CentralDeErros.Model.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CentralDeErros.API.Configuration
@@ -23,6 +27,30 @@ namespace CentralDeErros.API.Configuration
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                    ValidIssuer = appSettings.Issuer
+                };
+            });
 
             return services;
         }
